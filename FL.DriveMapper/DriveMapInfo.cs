f84778dcc9fd49e89f6545795b5f57fe3f165c94
Marco Stroppel <marco.stroppel@gmail.com>
2018-12-29 08:02:58 +0100

    Initial commit of first version.

---
+++ FL.DriveMapper/DriveMapInfo.cs
@@ -0,0 +1,47 @@
+﻿namespace FL.DriveMapper
+{
+    /// <summary>
+    /// Contains the information to map one drive.
+    /// </summary>
+    public class DriveMapInfo
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DriveMapInfo"/> class.
+        /// </summary>
+        /// <param name="drive">The drive.</param>
+        /// <param name="networkPath">The network path.</param>
+        /// <param name="label">The label.</param>
+        public DriveMapInfo(string drive, string networkPath, string label)
+        {
+            Drive = drive;
+            NetworkPath = networkPath;
+            Label = label;
+        }
+
+        /// <summary>
+        /// Gets the drive.
+        /// </summary>
+        public string Drive { get; private set; }
+
+        /// <summary>
+        /// Gets the network path.
+        /// </summary>
+        public string NetworkPath { get; private set; }
+
+        /// <summary>
+        /// Gets the label.
+        /// </summary>
+        public string Label { get; private set; }
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return typeof(DriveMapInfo).Name + ": Drive=" + Drive + " NetworkPath= " + NetworkPath + " Label=" + Label;
+        }
+    }
+}
---
+++ FL.DriveMapper/Program.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Security.Principal;
+
+namespace FL.DriveMapper
+{
+    /// <summary>
+    /// Contains the main entry point for the application.
+    /// </summary>
+    public class Program
+    {
+        private static readonly ILog _logSink = LogManager.GetLogger(typeof(Program));
+
+        private static readonly DriveMapInfo[] _driveMappings = new []
+        {
+            new DriveMapInfo("P:", @"\\rst-server\shared", "Test02")
+        };
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="args"></param>
+        public static int Main(string[] args)
+        {
+            foreach (var info in _driveMappings)
+            {
+                var mapper = new DriveMap(info);
+                mapper.MapDrive();
+            }
+            
+            return (int)ReturnCodes.Successful;
+        }
+    }
+}
